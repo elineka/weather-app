d6d267d16e2a2dde16cc097473bacaa2b9b6ea2d
unknown <you@example.com>
2020-09-12 16:17:42 +0300

    save weather searches on local storage in order to make the weather results persist on the page when a user reloads

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ let mySearch = document.getElementById('search');
 myButton.addEventListener('click', function () {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${mySearch.value}&lat=LATITUDE&lon=LONGITUDE&units=metric&APPID=ed51ad9f97704b89c72b2486d127a813`)
     .then(response => response.json())
-    .then(data =>{ localStorage.setItem('data', JSON.stringify(data))
+    .then(data =>{ localStorage.setItem('datat', JSON.stringify(data))
      console.log(data)
      
-document.getElementById('name').textContent = data.name
+document.getElementById('name').textContent = data.name;
+document.getElementById('country').textContent = data.sys.country;
 
 document.getElementById('temp').textContent = 'Temperature:' +" " + data.main.temp +'\u00B0C.';
 
@@ -35,9 +36,23 @@ document.getElementById('long').textContent = 'Longitude:'+" " + data.coord.lon 
   
 
 window.onload = function() {
-  // let search = localStorage.getItem('data')
-  // if(search) myParagraph.textContent = search
-  
+  let data = JSON.parse(localStorage.getItem('datat'))
+  if(data) {
+document.getElementById('name').textContent = data.name;
+document.getElementById('country').textContent = data.sys.country;
+
+document.getElementById('temp').textContent = 'Temperature:' +" " + JSON.parse(data.main.temp) +'\u00B0C.';
+
+let date =new Date(data.dt*1000);
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+let newDate = date.toLocaleString('en-US', options);
+
+document.getElementById('time').textContent =  newDate;
+document.getElementById('lati').textContent = 'Latitude:' +" " + data.coord.lat +'\u00B0';
+document.getElementById('long').textContent = 'Longitude:'+" " + data.coord.lon +'\u00B0';
+
+  } 
+ 
 }
 
   
